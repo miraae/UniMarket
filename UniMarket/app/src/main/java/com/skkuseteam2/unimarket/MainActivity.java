10894717e992182909d7bedd4df65413005fd287
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2019-06-27 14:18:07 +0900

    FireBase 와 Android Studio 업데이트 및 연동  + DatabaseReference 연결 부분 및 Listener

--- UniMarket/app/src/main/java/com/skkuseteam2/unimarket/MainActivity.java
+++ UniMarket/app/src/main/java/com/skkuseteam2/unimarket/MainActivity.java
@@ -3,11 +3,58 @@ package com.skkuseteam2.unimarket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("message");
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String strDate = sdf.format(c.getTime());
+
+        ChatModel user = new ChatModel(1,"홍길동", strDate, "메세지 갔습당", 01, 1);
+        mDatabase.push().setValue(user);
+
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                //DB내용 추가 혹은 업데이트 되었을시 이 함수가 호출이 됨.
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                 //DB가 변경 되었을 때
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+
     }
 }
