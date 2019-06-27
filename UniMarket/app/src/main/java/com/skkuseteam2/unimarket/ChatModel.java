6580c7f3ef9ab65b3b8d2f7aba332be06a56221f
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2019-06-27 13:26:27 +0900

    ChatModel 클라스 추가   (파이어 베이스에 추가, 업데이트 될 테이블 객체)
    - 생성자 -
     public ChatModel(int boardid,String name ,String timelog,String text, int picture , int otherid)

---
+++ UniMarket/app/src/main/java/com/skkuseteam2/unimarket/ChatModel.java
@@ -0,0 +1,26 @@
+package com.skkuseteam2.unimarket;
+
+public class ChatModel {
+
+    public int boardid;
+    public String name;
+    public String timelog;
+    public String text;
+    public int  picture;
+    public int otherid;
+
+    public ChatModel() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+
+    public ChatModel(int boardid,String name ,String timelog,String text, int picture , int otherid) {
+        this.name = name;
+        this.boardid; = boardid;
+        this.timelog = timelog;
+        this.text = text;
+        this.picture = picture;
+        this.otherid = otherid;
+    }
+
+}
