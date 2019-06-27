1ef7b823e9198cae3b38c4277a74366f0bf6703b
miraae <yes_future@naver.com>
2019-06-28 01:52:49 +0900

    detail, detailfind activity 버튼 연결

--- UniMarket/app/src/main/java/com/skkuseteam2/unimarket/DetailFindActivity.java
+++ UniMarket/app/src/main/java/com/skkuseteam2/unimarket/DetailFindActivity.java
@@ -2,6 +2,8 @@ package com.skkuseteam2.unimarket;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DetailFindActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class DetailFindActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_find);
+
+        ImageButton backBtn = (ImageButton)findViewById(R.id.backButton);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                overridePendingTransition(R.anim.anim_slide_in_left,R.anim.anim_slide_out_right);
+            }
+        });
     }
 }
