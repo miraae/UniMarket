17487afd84206aebc7bd1e64446966ff839db643
HongSeongKi <tjdrl558@naver.com>
2019-06-27 16:44:10 +0900

    페이지슬라이딩

--- UniMarket/app/src/main/java/com/skkuseteam2/unimarket/MainActivity.java
+++ UniMarket/app/src/main/java/com/skkuseteam2/unimarket/MainActivity.java
@@ -1,5 +1,6 @@
 package com.skkuseteam2.unimarket;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -22,7 +23,10 @@ public class MainActivity extends AppCompatActivity {
         menuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent intent =  new Intent(MainActivity.this,MyPageActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.anim_slide_in_left,R.anim.anim_slide_out_right);
+                finish();
             }
         });
 
