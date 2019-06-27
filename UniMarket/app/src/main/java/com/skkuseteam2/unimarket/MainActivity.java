7ff731e6ead70fcc20924034b76644dfaf54fa8e
miraae <yes_future@naver.com>
2019-06-27 09:09:00 +0900

    Detail Activity 추가 / main에서 실행하도록 함

--- UniMarket/app/src/main/java/com/skkuseteam2/unimarket/MainActivity.java
+++ UniMarket/app/src/main/java/com/skkuseteam2/unimarket/MainActivity.java
@@ -1,5 +1,6 @@
 package com.skkuseteam2.unimarket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +8,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // 삭제할 코드 (Detail Activity 먼저 실행)
+        Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+        startActivity(intent);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
